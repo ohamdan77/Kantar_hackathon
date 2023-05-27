75b9cc53e2b72ac4045209844c6d14628009daf8
ohamdan77 <ohamdan77>
2023-05-27 19:49:04 +0000

    Final

--- 1_tables_creation.py
+++ 1_tables_creation.py
@@ -9,3 +9,17 @@
 # MAGIC * Create 2 silver tables from the data:
 # MAGIC   * first one with all the features 
 # MAGIC   * second with the label in it
+# MAGIC
+# MAGIC
+# MAGIC
+# MAGIC In Case the data is not reachable from the S3 Bucket you can download it from Kaggle following the code below. Do not forget to change the username and the KAGGLE_KEY to be yours
+
+# COMMAND ----------
+
+# DBTITLE 0,In case the S3 data is not reachable you can download the data from Kaggle
+# MAGIC %sh 
+# MAGIC pip install kaggle
+# MAGIC export KAGGLE_USERNAME=omarhamdandata
+# MAGIC export KAGGLE_KEY=6fa85eea9baf2ea4c1682d7701910799
+# MAGIC kaggle datasets download -d mlg-ulb/creditcardfraud -p /dbfs/creditcard/data/
+# MAGIC cd /dbfs/creditcard/data/ &&  unzip /dbfs/creditcard/data/creditcardfraud.zip
